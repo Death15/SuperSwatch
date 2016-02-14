2cfe6c756cc95d063a8a4673c594cc463a115232
Death15 <arash.official15@gmail.com>
2016-02-14 13:47:58 -0800

    Rename Swatch-Manager.lua to SuperSwatch-Manager.lua

---
+++ plugins/SuperSwatch-Manager.lua
@@ -0,0 +1,43 @@
+local function set_bot_photo(msg, success, result)
+  local receiver = get_receiver(msg)
+  if success then
+    local file = 'data/photos/bot.jpg'
+    print('File downloaded to:', result)
+    os.rename(result, file)
+    print('File moved to:', file)
+    set_profile_photo(file, ok_cb, false)
+    send_large_msg(receiver, 'Photo changed!', ok_cb, false)
+    redis:del("bot:photo")
+  else
+    print('Error downloading: '..msg.id)
+    send_large_msg(receiver, 'Failed, please try again!', ok_cb, false)
+  end
+end
+local function parsed_url(link)
+  local parsed_link = URL.parse(link)
+  local parsed_path = URL.parse_path(parsed_link.path)
+  return parsed_path[2]
+end
+
+local function run(msg,matches)
+ local data = load_data(_config.moderation.data)
+    local receiver = get_receiver(msg)
+    local group = msg.to.id
+    if not is_admin(msg) then
+    	return
+    end
+    if msg.media then
+      	if msg.media.type == 'photo' and redis:get("bot:photo") then
+      		if redis:get("bot:photo") == 'waiting' then
+        		load_photo(msg.id, set_bot_photo, msg)
+      		end
+      	end
+    end
+    if matches[1] == "setprofile" then
+    	redis:set("bot:photo", "waiting")
+    	return 'Please send me profile photo now'
+    end
+    
+    if matches[1] == "setprofile" then
+    set_profile_name(matches[2],ok_cb,false)
+    return "Profile Name Changed To "..matches[2]
