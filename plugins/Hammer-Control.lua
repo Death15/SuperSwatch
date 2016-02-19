c73045a23ba05f61c549ea04d7fc25893bf919f5
Death15 <arash.official15@gmail.com>
2016-02-19 01:33:44 -0800

    Update Hammer-Control.lua

--- plugins/Hammer-Control.lua
+++ plugins/Hammer-Control.lua
@@ -5,12 +5,32 @@ if not is_sudo(msg) then
  return nil
  end
 
- if msg.to.type == 'channel'then
+ if msg.to.type == 'channel' then
  if matches[1] == "kick" then
 Channel = 'channel#'..msg.to.id
 user = 'user#'..matches[2]
  channel_kick_user(Channel, user, ok_cb, true)
- end
+end
+
+ if matches[1] == "id" then
+  if msg.to.type == "user" then
+   return "Your Name: "..msg.to.first_name.. "\n\nYour ID: "..msg.from.id.."\n\nYour Phone : "..msg.from.phone.."\n\nYour USername : "..msg.from.username
+  end
+  
+  if msg.to.type == 'channel' then
+   if matches[1] == 'id' then
+     local name = user_print_name(msg.from)
+     if result.phone then
+      phone = +"..result.phone
+     else
+      phone = 'None'
+     end
+     if result.username then
+      username = @"..result.username
+     else
+      username = 'None'
+      end
+    return "SuperGroup NAME : " ..string.gsub(msg.to.print_name, "_", " ").. "\nSuperGroup iD : "..msg.to.id.."\nYour ID : "..msg.from.id.."\nYour Phone : "..phone.."\nYour Username : "...username
 
 if matches[1] == "invite" then
 Channel = 'channel#'..msg.to.id
