31538a5eba6ef16487fc47a4491917759487c6c5
Death15 <arash.official15@gmail.com>
2016-02-19 01:58:58 -0800

    Create silentuser-option.lua

---
+++ plugins/silentuser-option.lua
@@ -0,0 +1,5 @@
+local function run(msg, matches)
+local hash_fa = fa:enabled:'msg.to.id
+local faenabled = redis:get(fash_fa)
+local hash = 'silentuser:'..matches[2]..':'..matches[3]..'..channel
+local user = "user#id"matches[2]
