3862d694a12cbfa96263d9e171d1946ac19a341d
Death15 <arash.official15@gmail.com>
2016-02-19 03:01:06 -0800

    Update setlang.lua

--- plugins/setlang.lua
+++ plugins/setlang.lua
@@ -1,24 +1,23 @@
 local function run(msg, matches)
-if not is_sudo(msg) then
-return "for Sudo Only"
+if not is_momod(msg) then
+return "for MOderators Only"
 end
 
-if matches[1] == "setlang" then
-local hash_per = 'persian:enabled:'..msg.to.id
-local perenabled = redis:get(hash_per)
+local hash_fa = 'fa:enabled:'..msg.to.id
+local faenabled = redis:get(hash_fa)
 if matches[2] == "per" then
   return "زبان گروه به فارسی تغیر کرد"
-  redis:set(hash_per, true)
+  redis:set(hash_fa, true)
 end
 
 local hash_en = 'english:enabled:'..msg.to.id
 local enenabled = redis:get(hash_en)
 if matches[2] == "en" then
   return "Group Language Has been set to : <English>"
-redis:set(hash_en, true)
+redis:del(hash_fa, true)
 end
 
-if not hash_per then
+if not hash_fa then
   redis:get(hash_en, true)
 end 
 end
