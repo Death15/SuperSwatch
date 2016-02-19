f94b2dd73376675f0c587db4cf4ce74aebf02ce0
Death15 <arash.official15@gmail.com>
2016-02-19 02:25:31 -0800

    Update silentuser-option.lua

--- plugins/silentuser-option.lua
+++ plugins/silentuser-option.lua
@@ -1,5 +1,33 @@
 local function run(msg, matches)
+  if not is_momod(msg) then
+    return ""
+  end
+  
 local hash_fa = fa:enabled:'msg.to.id
 local faenabled = redis:get(fash_fa)
 local hash = 'silentuser:'..matches[2]..':'..matches[3]..'..channel
 local user = "user#id"matches[2]
+if matches[1] == "silent" and matches[3] == "audio" then
+  redis:set(hash, true)
+if faenabled then
+  text1 = "یوزر از فرستادن ویس محروم شد"
+else
+  text1 = "User "..matches[2].." has been muted from sending audio"
+end
+return text1
+end
+if matches[1] "unsilent" and matches[3] == "audio" then
+  redis:del(hash)
+  if faenabled then
+    text2 = "یوزر مجاز به فرستادن ویس شد"
+  else
+    text2 = "User "..matches[2].." has been unmuted from sending audio"
+  end
+end
+return {
+  patterns = {
+    "^/(silent) (%d+) (audio)$",
+    "^/(unsilent) (%d+) (audio)$"
+    },
+  run = run
+  }
